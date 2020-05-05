1f9013d29655e23fa13583792a622bfa1adb730f
Erosinho13 <56429945+Erosinho13@users.noreply.github.com>
2020-05-05 12:14:07 +0200

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -6,54 +6,68 @@ import os
 import os.path
 import sys
 
+import pandas as pd
 
 def pil_loader(path):
-    # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
 
 
 class Caltech(VisionDataset):
-    def __init__(self, root, split='train', transform=None, target_transform=None):
-        super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
+    
+    def __init__(self, root, split = 'train', transform = None, target_transform = None):
+        
+        super(Caltech, self).__init__(root, transform = transform, target_transform = target_transform)
 
-        self.split = split # This defines the split you are going to use
-                           # (split files are called 'train.txt' and 'test.txt')
+        self.split = split
+        
+        avoid_folder = "BACKGROUND_Google"
+        
+        self._categories = sorted(os.listdir(root))
+        self._categories.remove(avoid_folder)
+        
+        self._labels_id = {label : i for i, label in enumerate(self._categories)}
+        
+        self._data = pd.DataFrame()
 
-        '''
-        - Here you should implement the logic for reading the splits files and accessing elements
-        - If the RAM size allows it, it is faster to store all data in memory
-        - PyTorch Dataset classes use indexes to read elements
-        - You should provide a way for the __getitem__ method to access the image-label pair
-          through the index
-        - Labels should start from 0, so for Caltech you will have lables 0...100 (excluding the background class) 
-        '''
+        with open(root + "/../" + split + ".txt", "r") as f:
+            content = f.readlines()
+        
+        labels = []
+        records = []
+        
+        for i, row in enumerate(content):
+            label, image_name = row[:-1].split("/")
+            if label != avoid_folder:
+                labels.append(self._labels_id[label])
+                records.append(pil_loader(root + "/" + row[:-1]))
 
+        self._data['image'] = records
+        self._data['label'] = labels
+    
+    
+    def getCategories(self):
+        return self._categories;
+    
+    
+    def getLabel(self, label):
+        return self._labels_id[label];
+    
+    
+    def getData(self):
+        return self._data;
+    
+    
     def __getitem__(self, index):
-        '''
-        __getitem__ should access an element through its index
-        Args:
-            index (int): Index
 
-        Returns:
-            tuple: (sample, target) where target is class_index of the target class.
-        '''
+        image, label = self._data.loc[index]['image'], self._data.loc[index]['label']
 
-        image, label = ... # Provide a way to access image and label via index
-                           # Image should be a PIL Image
-                           # label can be int
-
-        # Applies preprocessing when accessing the image
         if self.transform is not None:
             image = self.transform(image)
 
         return image, label
 
+    
     def __len__(self):
-        '''
-        The __len__ method returns the length of the dataset
-        It is mandatory, as this is used by several other components
-        '''
-        length = ... # Provide a way to get the length (number of elements) of the dataset
-        return length
+        return len(self._data)
